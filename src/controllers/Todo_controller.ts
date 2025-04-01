c11e297066e59234044862576e696a1bdc6f004d
williamlunelli <williamlunelli07@gmail.com>
2025-04-01 14:06:01 -0300

    Feat: Creating routes and list tasks

---
+++ src/controllers/Todo_controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import { Todo } from "../models/Todo";
+
+export const all = async (req: Request, res: Response) => {
+  const list = await Todo.findAll();
+  res.json({ list });
+};
+
+export const add = async (req: Request, res: Response) => {};
+
+export const update = async () => {};
+
+export const remove = async () => {};
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 
-import * as ApiController from "../controllers/apiController";
+import * as TodoController from "../controllers/Todo_controller";
 
 const router = Router();
 
-console.log("fix");
+router.get("/todo", TodoController.all);
+router.post("/todo", TodoController.add);
+router.put("/todo/:id", TodoController.update);
+router.delete("/todo/:id", TodoController.remove);
 
 export default router;
