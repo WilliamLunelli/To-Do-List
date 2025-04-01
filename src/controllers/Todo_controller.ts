f67a76727a8fdd0bf95f2af52fd0e5a32871d0e0
williamlunelli <williamlunelli07@gmail.com>
2025-04-01 14:38:52 -0300

    Feat: Creating delete and update

--- src/controllers/Todo_controller.ts
+++ src/controllers/Todo_controller.ts
@@ -19,6 +19,39 @@ export const add = async (req: Request, res: Response) => {
   res.json({ error: "Dados não enviados" });
 };
 
-export const update = async () => {};
+export const update = async (req: Request, res: Response) => {
+  const id: string = req.params.id;
+  let todo = await Todo.findByPk(id);
+  if (todo) {
+    if (req.body.title) {
+      todo.title = req.body.title;
+    }
+    if (req.body.done) {
+      switch (req.body.done.toLowerCase()) {
+        case "true":
+        case "1":
+          todo.done = true;
+          break;
+        case "false":
+        case "0":
+          todo.done = false;
+          break;
+      }
+    }
 
-export const remove = async () => {};
+    await todo.save();
+    res.json({ item: todo });
+  } else {
+    res.json({ error: "Item não encontrado" });
+  }
+};
+
+export const remove = async (req: Request, res: Response) => {
+  let id: string = req.params.id;
+  let todo = await Todo.findByPk(id);
+
+  if (todo) {
+    await todo.destroy();
+  }
+  res.json({ Status: "Excluido" });
+};
